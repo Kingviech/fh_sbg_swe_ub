7f92520b5d4302bf84cd8d384da11158ba21b20b
Markus Ziegler <mziegler.itsb-b2013@fh-salzburg.ac.at>
2013-10-14 15:16:55 +0200

    initial commit

---
+++ TEST_GIT_3/src/TEST_GIT_3.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : TEST_GIT_3.c
+ Author      : Markus Ziegler
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
